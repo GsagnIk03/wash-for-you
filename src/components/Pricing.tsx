75d18630f2b5278431e4fceb102a4311355ef0f9
Sayani Bag Sharma <sayanibagsharma@gmail.com>
2026-04-12 20:16:21 +0530

    TASK-0008: Removed useState

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useInView } from "../hooks";
 import { PRICING_PLANS } from "../data";
 import type { PricingPlan } from "../types";
